541c9cf6c215e8a4392864d60b6456b6f4239e80
Kevin Mangan <kevinmangan13@gmail.com>
2012-01-28 15:37:28 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrowdServant::Application.config.secret_token = 'b9518dbb444b5f623a9b236690f6f2ca868d8f03a8f6f76dfc1b6f9a0c8129cca3aa5bbc71f6d348fa4c90da476a66e8975b16b053dbb2f957ca6508ffd7fcbf'
